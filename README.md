1d65ce5131385dead9ad25fab20a96d88fd55906
bala <bala@redhat.com>
2024-11-02 13:47:31 +0800

    "updates to make it work locally"

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
+Reference: https://github.com/wildfly/quickstart/tree/main/ejb-remote
+
+Local testing
+==============
+1. Start EAP 8 on the local machine
+```
+cd /Users/bbalasub/Mywork/Setup/eap8/bin
+./standalone.sh
+```
+
+2. Clone this repository. Package the application and deploy to 
+```
+mvn clean package
+cp target/eap8-simple.war ~/Mywork/Setup/eap8/standalone/deployments
+```
+
+3. Check EAP 8 logs to see that the application has been deployed successfully and the JNDI names of the deployed EJB is shown clearly. Check if the application is accessible at the following URL:
+```
+curl -v http://localhost:8080/eap8-simple/TestServlet
+```
+
+4.  Test the remote EJB access from the local client using the integration tests
+```
+mvn verify -Pintegration-testing
+```
+
+Deploying on EAP 8 in OpenShift
+=================================
 1. Add EAP8 Helm charts to the OpenShift cluster
 ```
 helm repo add jboss-eap https://jbossas.github.io/eap-charts/
@@ -10,7 +38,7 @@ mvn clean package -Popenshift -DskipTests
 
 3. Access the application at the following URL 
 ```
-http://localhost:8080/eap8-simple-8.0.3/HelloWorld
+http://localhost:8080/eap8-simple/TestServlet
 ```
 
 4. Run remote EJB integration testing
--- src/main/java/com/bala/eap/TestServlet.java
+++ src/main/java/com/bala/eap/TestServlet.java
@@ -19,43 +19,25 @@ package com.bala.eap;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * <p>
- * A simple servlet taking advantage of features added in 3.0.
- * </p>
- *
- * <p>
- * The servlet is registered and mapped to /HelloServlet using the {@linkplain WebServlet
- * @HttpServlet}. The {@link HelloService} is injected by CDI.
- * </p>
- *
- * @author Pete Muir
- *
- */
-@SuppressWarnings("serial")
-@WebServlet("/HelloWorld")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
 
-    static String PAGE_HEADER = "<html><head><title>helloworld</title></head><body>";
+    static String PAGE_HEADER = "<html><head><title>Test Servlet</title></head><body>";
 
     static String PAGE_FOOTER = "</body></html>";
-
-    @Inject
-    HelloService helloService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        writer.println("Test Servlet");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
